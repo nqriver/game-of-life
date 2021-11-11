dfa786531f52c06a390935f3d546a9dcd8c7521d
nqriver <nqriver@gmail.com>
2021-11-11 01:29:36 +0100

    finding neighbours logic

--- src/main/java/com/myprojects/gameoflife/model/Grid.java
+++ src/main/java/com/myprojects/gameoflife/model/Grid.java
@@ -1,5 +1,10 @@
 package com.myprojects.gameoflife.model;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static com.myprojects.gameoflife.model.Utils.requirePositiveInteger;
+
 public class Grid {
 
     private final int rowsNumber;
@@ -8,8 +13,8 @@ public class Grid {
     private final Cell[][] cells;
 
     public Grid(int rowsNumber, int colsNumber) {
-        this.rowsNumber = rowsNumber;
-        this.colsNumber = colsNumber;
+        this.rowsNumber = requirePositiveInteger(rowsNumber, "Number of rows should be positive");
+        this.colsNumber = requirePositiveInteger(colsNumber, "Number of columns should be positive");
         cells = initCells();
     }
 
@@ -21,6 +26,19 @@ public class Grid {
         return colsNumber;
     }
 
+    private int getWrappedColNumber(int colsNumber) {
+        return (colsNumber + rowsNumber) % rowsNumber;
+    }
+
+
+    private int getWrappedRowNumber(int rowNumber) {
+        return (rowNumber + rowsNumber) % rowsNumber;
+    }
+
+    private Cell getCell(int rowIndex, int colIndex) {
+        return cells[getWrappedRowNumber(rowIndex)][getWrappedColNumber(colIndex)];
+    }
+
     private Cell[][] initCells() {
         Cell[][] cells = new Cell[getRowsNumber()][getColsNumber()];
         for (int rowIndex = 0; rowIndex < getRowsNumber(); rowIndex++) {
@@ -29,5 +47,23 @@ public class Grid {
             }
         }
         return cells;
+    }
+
+    private List<Cell> getNeighbours(int rowIndex, int colIndex) {
+        int north = rowIndex - 1;
+        int south = rowIndex + 1;
+        int east = colIndex + 1;
+        int west = colIndex - 1;
+
+        return List.of(
+                getCell(north, colIndex),
+                getCell(north, east),
+                getCell(rowIndex, east),
+                getCell(south, east),
+                getCell(south, colIndex),
+                getCell(south, west),
+                getCell(rowIndex, west),
+                getCell(north, west)
+        );
     }
 }

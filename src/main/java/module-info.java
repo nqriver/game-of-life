c917fb69f03bb93cf26252bbe475c44c0fe4487e
nqriver <nqriver@gmail.com>
2021-11-14 17:09:48 +0100

    tidy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ module com.myprojects.gameoflife {
 
     opens com.myprojects.gameoflife to javafx.fxml;
     exports com.myprojects.gameoflife;
+
+    exports com.myprojects.gameoflife.controller to javafx.fxml;
+    opens com.myprojects.gameoflife.controller to javafx.fxml;
 }

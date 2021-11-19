4d89c9e670b85a8eae4287abfc8ddc92ab110832
nqriver <nqriver@gmail.com>
2021-11-19 02:47:07 +0100

    speed values changed

--- src/main/java/com/myprojects/gameoflife/model/GenerationSpeed.java
+++ src/main/java/com/myprojects/gameoflife/model/GenerationSpeed.java
@@ -1,9 +1,9 @@
 package com.myprojects.gameoflife.model;
 
 public enum GenerationSpeed {
-    SLOW(500),
-    MEDIUM(300),
-    FAST(100);
+    SLOW(300),
+    MEDIUM(150),
+    FAST(50);
 
     private final double millisecondsBetweenGenerations;
 
